2004bfa5a94aacfd161590635e58b79877dd41bf
FelipeCastro2021 <felipe2021castro@gmail.com>
2024-08-15 10:32:36 -0300

    ForEach imprimindo todos os elementos dentro do array.

--- teste/src/academy/devdojo/maratonajava/introducaoExercicios/ForEachImprimirArray.java
+++ teste/src/academy/devdojo/maratonajava/introducaoExercicios/ForEachImprimirArray.java
@@ -3,8 +3,17 @@ package academy.devdojo.maratonajava.introducaoExercicios;
 public class ForEachImprimirArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		Descrição: Escreva um programa que use forEach para imprimir todos os elementos de um array de inteiros.
+//		Exemplo: Se o array for {10, 20, 30}, o programa deve imprimir:
+//		10
+//		20
+//		30
 
+		int[] array = {10, 20, 30};
+		
+		for (int i : array) {
+			System.out.println(i);
+		}
 	}
 
 }
